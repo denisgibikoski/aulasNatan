a3d45d1b23fcb6141e177b022128bcd99a401397
Denis Gibikoski <d3nys.inf@outlook.com>
2018-01-25 20:23:44 -0200

    PrimeiroGit

---
+++ src/main/java/br/com/denis/controller/ExemploController.java
@@ -0,0 +1,21 @@
+package br.com.denis.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class ExemploController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String home (){
+		return "Ola Denis!!!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploController.class, args);
+	}
+}
